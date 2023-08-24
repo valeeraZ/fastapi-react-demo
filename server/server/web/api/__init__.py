e28f6ee7b55fe5c51bff7507e044ba0c7c4e0c80
Wenzhuo Zhao <zslyvain@gmail.com>
2023-08-24 12:36:58 +0200

    feat(server): healthcheck
    add healthcheck route

--- server/server/web/api/__init__.py
+++ server/server/web/api/__init__.py
@@ -1,6 +1,7 @@
 """API for the application."""
 from fastapi.routing import APIRouter
-from server.web.api import contact
+from server.web.api import contact, monitoring
 
 api_router = APIRouter()
 api_router.include_router(contact.router)
+api_router.include_router(monitoring.router)
