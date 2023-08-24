f4038cc0d56d10cf2acd4aa5ee3a6496769b3ac7
Wenzhuo Zhao <zslyvain@gmail.com>
2023-08-24 12:22:27 +0200

    fix(server): cors
    add middleware for cors

--- server/server/web/application.py
+++ server/server/web/application.py
@@ -1,6 +1,7 @@
 from importlib import metadata
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import UJSONResponse
 from server.exception.base_exception_handler import (
     NotCreatedError,
@@ -36,6 +37,19 @@ def get_app() -> FastAPI:
     # Adds startup and shutdown events.
     register_startup_event(app)
     register_shutdown_event(app)
+
+    origins = [
+        "http://localhost",
+        "http://localhost:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     # Main router for the API.
     app.include_router(router=api_router, prefix="/api")
